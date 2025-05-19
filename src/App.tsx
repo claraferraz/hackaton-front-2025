5a9c93aff1d5df09d0829f32446afad930bb6bb2
Clara Ferraz <claraferraz@proton.me>
2025-05-19 11:20:09 -0300

    instalação do vite, react-router-dom e mui

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/home";
+import Home from "./pages/home/Home";
 import Header from "./components/header";
 
 const App = () => {
-
   return (
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </BrowserRouter>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
