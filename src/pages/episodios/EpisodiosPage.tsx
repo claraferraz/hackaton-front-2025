172327c61ef2700fcbc82db781feff3997584571
Clara Ferraz <claraferraz@proton.me>
2025-05-20 15:18:51 -0300

    select de temporadas ainda nao funciona

--- src/pages/episodios/EpisodiosPage.tsx
+++ src/pages/episodios/EpisodiosPage.tsx
@@ -5,9 +5,18 @@ import {
   CardEpisodios,
   type EpisodiosAPI,
 } from "../../components/CardEpisodios/CardEpisodios";
+import { MenuItem, Select } from "@mui/material";
 
 export const EpisodiosPage = () => {
+  const [temporada, setTemporada] = useState<string>("Temporada 1");
   const [episodios, setEpisodios] = useState<EpisodiosAPI[]>([]);
+  const temporadasList = [
+    "Temporada 1",
+    "Temporada 2",
+    "Temporada 3",
+    "Temporada 4",
+    "Temporada 5",
+  ];
 
   useEffect(() => {
     getEpisodes().then((response) => {
@@ -18,6 +27,15 @@ export const EpisodiosPage = () => {
   return (
     <div>
       <H1>Episodios</H1>
+      <Select
+        value={temporada}
+        onChange={(e) => setTemporada(e.target.value)}
+        fullWidth
+      >
+        {temporadasList.map((option) => (
+          <MenuItem value={option}>{option}</MenuItem>
+        ))}
+      </Select>
       {episodios.length > 0 &&
         episodios.map((episodio) => (
           <CardEpisodios key={episodio.id} {...episodio} />
